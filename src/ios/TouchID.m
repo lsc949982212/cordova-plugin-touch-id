9513b7694bb1e40c79bda0fb065b59b289f62c7d
lsc949982212 <949982212@qq.com>
2019-12-16 18:05:47 +0800

    first commit

--- src/ios/TouchID.m
+++ src/ios/TouchID.m
@@ -38,6 +38,74 @@ NSString *keychainItemServiceName;
   }];
 }
 
+- (void) authenticate:(CDVInvokedUrlCommand*)command;
+{
+    NSString *text = [command.arguments objectAtIndex:0];
+
+    __block CDVPluginResult* pluginResult = nil;
+
+    if (NSClassFromString(@"LAContext") != nil)
+    {
+        LAContext *laContext = [[LAContext alloc] init];
+        NSError *authError = nil;
+
+        if ([laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&authError])
+        {
+            [laContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:text reply:^(BOOL success, NSError *error)
+             {
+                 if (success)
+                 {
+                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                 }
+                 else
+                 {
+                     NSString *errorCode = nil;
+                     switch (error.code) {
+                         case LAErrorAuthenticationFailed:
+                             errorCode = @"authenticationFailed";
+                             break;
+                         case LAErrorUserCancel:
+                             errorCode = @"userCancel";
+                             break;
+                         case LAErrorUserFallback:
+                             errorCode = @"userFallback";
+                             break;
+                         case LAErrorSystemCancel:
+                             errorCode = @"systemCancel";
+                             break;
+                         case LAErrorPasscodeNotSet:
+                             errorCode = @"passcodeNotSet";
+                             break;
+                         case LAErrorTouchIDNotAvailable:
+                             errorCode = @"touchIDNotAvailable";
+                             break;
+                         case LAErrorTouchIDNotEnrolled:
+                             errorCode = @"touchIDNotEnrolled";
+                             break;
+                         default:
+                             errorCode = @"unknown";
+                             break;
+                     }
+                     
+                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorCode];
+                 }
+
+                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+             }];
+        }
+        else
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[authError localizedDescription]];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+    }
+    else
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
+}
+
 - (void) didFingerprintDatabaseChange:(CDVInvokedUrlCommand*)command {
   // Get enrollment state
   [self.commandDelegate runInBackground:^{
